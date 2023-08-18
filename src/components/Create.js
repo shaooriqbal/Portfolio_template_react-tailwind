e85643438fb790ddacd92dda2768d12cddd4aca4
shaoor <shaooriqbal786@gmail.com>
2023-08-18 22:07:06 +0500

    react mui init + routes.

---
+++ src/components/Create.js
@@ -0,0 +1,22 @@
+import { Typography } from '@mui/material'
+import React from 'react'
+
+export default function CreateComponent() {
+  return (
+      <div>
+          
+          <Typography
+              variant='h6'
+              component='h2'
+              gutterBottom
+              color='secondary'
+              align='center'
+            
+          >
+              Create a new Note
+          </Typography>
+
+
+    </div>
+  )
+}
