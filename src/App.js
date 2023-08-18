1a20be401e07a6fa1b10bc89b8b2a0f88e89183a
shaoor <shaooriqbal786@gmail.com>
2023-08-18 22:08:07 +0500

    routes and styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { createBrowserRouter , createRoutesFromElements ,Route, RouterProvider } from 'react-router-dom';
+import NotesComponent from './components/Notes';
+import CreateComponent from './components/Create';
+import HomeComponent from './components/Home';
+
 
 function App() {
+  
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<HomeComponent />}>
+        
+        <Route path='notes' element={ <NotesComponent/>}></Route>
+        <Route path='create' element={ <CreateComponent/>}></Route>
+      
+      
+      </Route>
+    )
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <RouterProvider router={router}></RouterProvider>
   );
 }
 
